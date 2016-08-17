9a3bf5ad1579fc2edebb4e43871b18afd9dcab29
Kris Hicks <khicks@pivotal.io>
2016-08-17 10:47:27 -0700

    Simplify prefixedWriter.Write()
    - Remove some nested ifs
    - Remove duplicate PrintBlock calls (6 calls -> 3)
    
    With this change it makes it clear that it only prints something if the
    current piece has content or if it's the last piece.

--- ui/combo_writer.go
+++ ui/combo_writer.go
@@ -32,23 +32,19 @@ func (s prefixedWriter) Write(bytes []byte) (int, error) {
 	pieces := strings.Split(string(bytes), "\n")
 
 	for i, piece := range pieces {
-		if i == len(pieces)-1 {
-			if s.w.onNL {
-				if len(piece) > 0 {
-					s.w.ui.PrintBlock(s.prefix)
-					s.w.ui.PrintBlock(piece)
-					s.w.onNL = false
-				}
-			} else {
-				s.w.ui.PrintBlock(piece)
-			}
-		} else {
+		if i < len(pieces)-1 || len(piece) > 0 {
 			if s.w.onNL {
 				s.w.ui.PrintBlock(s.prefix)
 			}
+
 			s.w.ui.PrintBlock(piece)
-			s.w.ui.PrintBlock("\n")
-			s.w.onNL = true
+
+			if i < len(pieces)-1 {
+				s.w.ui.PrintBlock("\n")
+				s.w.onNL = true
+			} else {
+				s.w.onNL = false
+			}
 		}
 	}
 
